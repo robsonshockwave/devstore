baacf133539bef775c2322dd6046d08326478ed2
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-04-30 22:45:14 -0300

    criando Fetch API wrapper

---
+++ src/env.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod';
+
+const envSchema = z.object({
+  NEXT_PUBLIC_API_BASE_URL: z.string().url(),
+});
+
+const parsedEnv = envSchema.safeParse(process.env);
+
+if (!parsedEnv.success) {
+  console.error(
+    'Environment validation error:',
+    parsedEnv.error.flatten().fieldErrors
+  );
+
+  throw new Error('Environment validation error');
+}
+
+export const env = parsedEnv.data;
